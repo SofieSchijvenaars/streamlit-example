87981ac1caa9a27769ddd0479c8320e9e91e3e64
SofieSchijvenaars <59667256+SofieSchijvenaars@users.noreply.github.com>
2023-01-16 12:56:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import numpy as np
 
 """
 # Welcome to Streamlit!
@@ -15,7 +16,7 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-
+st.write('TEST')
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
